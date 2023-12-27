f7438ab215ce864571dbdb3d3ad5561d2a854aa1
Akshay V M <akshayvm36@gmail.com>
2023-12-27 15:52:00 +0530

    Display staged

---
+++ src/componenets/Display.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Display({findResult,copy,result }) {
+  return (
+    <div className="w-full h-1/4 rounded-md my-2 p-2 border-solid border-white border-2 relative">
+      <div className="w-full h-full rounded-xl grid grid-cols-2 grid-rows-2 text-xl font-bold text-font_white p-6">
+        <div className="col-start-1 col-end-2 text-clr_blue">{`${copy ? "=" : ""}`}</div>
+        <div className="col-start-2 col-end-3 row-start-1 row-end-2 absolute right-4">
+          {`${copy ? copy : ""}`}
+        </div>
+
+        <div className="col-start-2 col-end-3 row-start-2 row-end-3 absolute bottom-2 right-4">
+          {result}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Display;
